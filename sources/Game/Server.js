4965e9d02c2447497b7a80036fc1addf4bb76b46
brunosimon <simon.bruno.77@gmail.com>
2025-12-04 14:29:12 +0100

    :sparkles: Server > Add notifications

--- sources/Game/Server.js
+++ sources/Game/Server.js
@@ -1,11 +1,14 @@
 import msgpack from 'msgpack-lite'
 import { v4 as uuidv4 } from 'uuid'
 import { Events } from './Events.js'
+import { Game } from './Game.js'
 
 export class Server
 {
     constructor()
     {
+        this.game = Game.getInstance()
+
         // Unique session ID
         this.uuid = localStorage.getItem('uuid')
         if(!this.uuid)
@@ -51,12 +54,42 @@ export class Server
                 this.onReceive(message)
             })
 
+            // Notification (only if been running for a while)
+            if(this.game.ticker.elapsed > 10)
+            {
+                const html = /* html */`
+                    <div class="top">
+                        <div class="title">Server connected</div>
+                    </div>
+                `
+
+                this.game.notifications.show(
+                    html,
+                    'server-connected',
+                    8
+                )
+            }
+
             // On close
             this.socket.addEventListener('close', () =>
             {
                 document.documentElement.classList.add('is-server-offline')
                 document.documentElement.classList.remove('is-server-online')
                 this.connected = false
+
+                // Notification
+                const html = /* html */`
+                    <div class="top">
+                        <div class="title">Server disconnected</div>
+                    </div>
+                `
+
+                this.game.notifications.show(
+                    html,
+                    'server-disconnected',
+                    8
+                )
+                
                 this.events.trigger('disconnected')
             })
         })
