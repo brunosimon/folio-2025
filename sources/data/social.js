4e647b9e891e0b1077711dc4c9d28e5a840d5fab
yan04s <yan04s@users.noreply.github.com>
2026-04-18 03:04:40 +0000

    feat: Refine GitHub Portfolio with Personal Info
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- sources/data/social.js
+++ sources/data/social.js
@@ -1,10 +1,5 @@
 export default [
-    { name: 'X', url: 'https://x.com/bruno_simon', align: 'right' },
-    { name: 'Bluesky', url: 'https://bsky.app/profile/bruno-simon.bsky.social', align: 'right' },
-    { name: 'Youtube', url: 'https://www.youtube.com/@BrunoSimon', align: 'right' },
-    { name: 'Mail', url: 'mailto:simon.bruno.77@gmail.com', align: 'right' },
-    { name: 'Twitch', url: 'https://www.twitch.tv/bruno_simon_dev', align: 'right' },
-    { name: 'GitHub', url: 'https://github.com/brunosimon', align: 'right' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/simonbruno77/', align: 'left' },
-    { name: 'Discord', modal: 'discord', align: 'left' },
+    { name: 'GitHub', url: 'https://github.com/yan04s', align: 'right' },
+    { name: 'LinkedIn', url: 'https://linkedin.com/in/yan825', align: 'left' },
+    { name: 'Mail', url: 'mailto:tan828825@gmail.com', align: 'right' },
 ]
